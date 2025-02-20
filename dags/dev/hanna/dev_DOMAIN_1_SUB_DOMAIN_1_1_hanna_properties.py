536e5e7fd9aac1054bf37683dcfcc8c8b857b954
Jorge Adrian Puc Carrillo <japuc@grupoasesores.com.mx>
2025-02-20 17:50:25 -0600

    Templates updated 2025-02-20 17:50:25

--- dags/dev/hanna/dev_DOMAIN_1_SUB_DOMAIN_1_1_hanna_properties.py
+++ dags/dev/hanna/dev_DOMAIN_1_SUB_DOMAIN_1_1_hanna_properties.py
@@ -22,7 +22,7 @@ from airflow.operators.python_operator import PythonOperator
 """
 
 job_name = "devhanna"
-pipeline_id = 62
+pipeline_id = 63
 pipeline_name = 'dev_DOMAIN_1_SUB_DOMAIN_1_1_hanna_properties'    
 flex_template = 'hana_flex_template.json'        
 stage = 'dev'
