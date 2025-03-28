518df3d58e694dc72ec662960fd6f95cbe466174
spring <spring@localhost>
2025-03-28 13:47:42 -0600

    Templates updated 2025-03-28 13:47:42

--- DOMAIN_1/SUB_DOMAIN_1_1/api/job21dev.py
+++ DOMAIN_1/SUB_DOMAIN_1_1/api/job21dev.py
@@ -44,7 +44,7 @@ api_method = 'GET'
 api_host = 'qat-api.liverpool.com.mx/api/ordermanagement/orders'
 api_headers = '[{"name":"apikey", "type":"secret", "value":"orders_apikey"}]'
 api_port = ''
-api_pathparams = ' '
+api_pathparams = ''
 api_queryparams = '[{"name":"start_update_date", "type":"dynamic", "value":""}, {"name":"end_update_date", "type":"dynamic", "value":""}, {"name":"size", "type":"static", "value":"200"}]'
 api_protocol = 'https'
 
