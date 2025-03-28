df24b70eea6ce420e615183db5f3fe61decf9bad
spring <spring@localhost>
2025-03-28 12:54:08 -0600

    Templates updated 2025-03-28 12:54:08

--- DOMAIN_1/SUB_DOMAIN_1_1/api/job25dev.py
+++ DOMAIN_1/SUB_DOMAIN_1_1/api/job25dev.py
@@ -41,7 +41,7 @@ network_tag = '<NETWORK_TAG>'
 
 #Datos Especificos
 api_method = 'GET'
-api_host = 'aae.ngrok.dev'
+api_host = 'aae.ngrok.dev/api/ordermanagement/orders'
 api_headers = '[{"name":"apikey", "type":"secret", "value":"orders_apikey"}]'
 api_port = ''
 api_pathparams = ' '
