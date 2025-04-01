64718fe6b8ca1d35a0f42615bf7506ef77ed3234
spring <spring@localhost>
2025-04-01 16:34:50 -0600

    Templates updated 2025-04-01 16:34:50

--- DOMAIN_1/SUB_DOMAIN_1_1/api/job1dev.py
+++ DOMAIN_1/SUB_DOMAIN_1_1/api/job1dev.py
@@ -36,7 +36,7 @@ target_dataset = 'demo_dwh_bkt'
 datetime_start = ''
 service_account = '457816054800-compute@developer.gserviceaccount.com'
 subnetwork = 'projects/labuniformes/regions/us-central1/subnetworks/default'
-region = 'us-east4'
+region = 'us-central1'
 network_tag = '<NETWORK_TAG>'
 
 #Datos Especificos
